27c4dafa75e736e08bf2530e2f72315bfa7e8bcb
sofiarizap <sofiaariza15@hotmail.com>
2023-04-14 19:11:23 -0500

    proyectos

--- script/index.js
+++ script/index.js
@@ -1,11 +1,43 @@
-window.addEventListener ('scroll', function (){
-    let animacion = document .getElementById( 'animado') ;
+window.addEventListener ('mousemove', function (){
+    let animacion = document.getElementById('animado') ;
     let posicionobj1 = animacion.getBoundingClientRect().top;
-    console.log (posicion0bj1);
     let tamañoDePantalla = window.innerHeight;
-    if (posicionobj1 < tamañoDePantalla){
-        animacion.style.animation ='mover 1s ease-out'
+    if (posicionobj1 <= tamañoDePantalla){
+        animacion.style.animation ='mover 2s ease-out'
     }
 })  
+,
+window.addEventListener ('scroll', function (){
+    let animacion = document.getElementById('animado1','animado3') ;
+    let posicionobj1 = animacion.getBoundingClientRect().top;
+    let tamañoDePantalla = window.innerHeight;
+    if (posicionobj1 < tamañoDePantalla){
+        animacion.style.animation ='mover1 1.2s ease-in'
+    }
+}),
+window.addEventListener ('scroll', function (){
+    let animacion = document.getElementById('animado2','animado4') ;
+    let posicionobj1 = animacion.getBoundingClientRect().top;
+    let tamañoDePantalla = window.innerHeight;
+    if (posicionobj1 <= tamañoDePantalla){
+        animacion.style.animation ='mover 1.2s ease-out'
+    }
+}),
+window.addEventListener ('scroll', function (){
+    let animacion = document.getElementById('animado3') ;
+    let posicionobj1 = animacion.getBoundingClientRect().top;
+    let tamañoDePantalla = window.innerHeight;
+    if (posicionobj1 < tamañoDePantalla/4){
+        animacion.style.animation ='mover1 1.2s ease-in'
+    }
+}),
+window.addEventListener ('scroll', function (){
+    let animacion = document.getElementById('animado4') ;
+    let posicionobj1 = animacion.getBoundingClientRect().top;
+    let tamañoDePantalla = window.innerHeight;
+    if (posicionobj1 <= tamañoDePantalla/5){
+        animacion.style.animation ='mover 1.2s ease-out'
+    }
+})
     
     
